6aa57c3f33e41029b1067f35cd1b17aca75ce21e
Clemens Katzer <clemens@cleka.net>
2020-02-29 14:22:38 +0200

    Main page layout (using <table>): main iterinary left, upcoming connections right

--- src/App.js
+++ src/App.js
@@ -67,14 +67,34 @@ function starttime_hhmmss(time_ms_epoch)
 }
 
 
+class MainIterinary extends Component {
+
+  constructor(props) {
+      super();
+
+  }
+
+  render(){
+
+    return (
+      <div className="MainIterinary">
+        <p>
+	    This is the main iterinary.
+	</p>
+      </div>
+    );
+  } 
+} 
+
 const Iterinary = () => (
     <Query query={iterinaryQuery}>
        {( {loading, error, data } ) => { 
            if (loading) return <p>Loading... </p>;
            if (error)   return <p>An error occured fetching the data, try again later.</p>;
 
+	   // ==================== Main Iterinary (on the left) ====================
+	   // TODO: display all a bit bigger, using CSS ?
 	   const it0 = data.plan.itineraries[0];
-
 	   
 	   const walkdist_raw = Math.round(it0.walkDistance-50);
 	   const walkdist = rounded(walkdist_raw);
@@ -89,7 +109,23 @@ const Iterinary = () => (
 
 	   let starttime_hr = starttime_hhmmss(startTime);
 
-           return  <div><p>Total walk distance is {walkdist}{walkdist_detailed}, total duration is {duration}</p><p>Start time {starttime_hr}, time left until leave {minsLeftUntilLeave} </p></div>
+	   // TODO: build it from reusable components instead?
+	   let myMainIterinary = <div><h1>Next connection</h1><p>Total walk distance is {walkdist}{walkdist_detailed}, 
+	   total duration is {duration}</p>
+	       <p>Start time {starttime_hr}, time left until leave {minsLeftUntilLeave} </p></div>;
+
+
+	   // ==================== Build it all together ====================
+
+	   // Build the whole page. As first hack, table to show main iterinary left, and summaries of upcoming
+	   // following iterinaries on the right. Later perhaps use floating "div" elements with CSS?
+	   const mainPageLayout = <table><tr>
+	       <td valign='top' width='50%'>
+  	         {myMainIterinary}</td>
+	       <td width='10%'></td>
+	       <td valign='top' width='40%'><p><h2>Upcoming connections</h2></p></td></tr></table>;
+           return mainPageLayout;
+
        } } 
 
     </Query>
@@ -105,20 +141,12 @@ class InfoScreen extends Component {
       this.state = { minutesLeft: 30 };
   }
 
-
   render(){
     return (
       <div className="InfoScreen">
         <p>
 	    <Iterinary></Iterinary>
 	</p>
-	    <p>
-	    Route info
-	</p>
-	    <p>
-	    Destination info
-	</p>
-
       </div>
     );
   } 
@@ -126,14 +154,17 @@ class InfoScreen extends Component {
 
 
 
+//	fromPlace: "Pohjoinen Rautatienkatu 25, Helsinki::60.1693803,24.9236575",
+//	toPlace: "Laurinniityntie 20, Helsinki::60.2318774,24.8733656",
+
+//	date: "2020-02-29",
+//	time: "14:10:00",
 
 const iterinaryQuery = gql`
 {
     plan(
-	fromPlace: "Pohjoinen Rautatienkatu 25, Helsinki::60.1693803,24.9236575",
-	toPlace: "Laurinniityntie 20, Helsinki::60.2318774,24.8733656",
-	date: "2020-02-29",
-	time: "13:10:00",
+	fromPlace: "Pohjoinen Rautatienkatu 25::60.1693803,24.9236575",
+	toPlace: "Laurinniityntie 20::60.2318774,24.8733656",
 	numItineraries: 3,
 	walkReluctance: 10.0,
 	minTransferTime: 180,
